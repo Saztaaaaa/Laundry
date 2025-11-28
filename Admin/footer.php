ab312501e2a9abd4ef3e118d97c56d4c4df2f3e6
Saztaaaaa <agitaviana53@gmail.com>
2025-11-28 09:08:52 +0700

    Initial commit

---
+++ Admin/footer.php
@@ -0,0 +1,50 @@
+<br>
+<style>
+  body {
+    background-color: #f0f2f5; 
+    font-family: Arial, sans-serif;
+  }
+
+  .container {
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+  }
+</style>
+<footer class="footer bg-dark text-light pt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 md-3">
+        <h5 class="text-uppercase"><b>Sistem Informasi Laundry</b></h5>
+        <p>
+          Membantu mengelola data pelanggan, transaksi, dan cucian
+          agar lebih cepat dan terorganisir.
+        </p>
+      </div>
+      <div class="col-md-4 md-3">
+       <h5><b>Jam Operasional</b></h5>
+    <p><i class="glyphicon glyphicon-time"></i> Senin – Sabtu: 08.00 – 18.00</p>
+    <i class="glyphicon glyphicon-time"></i> Minggu: Tutup</p>
+      </div>
+      <div class="col-md-4 md-3">
+        <h5 class="text-uppercase"><b>Ikuti Kami</b></h5>
+        <a href="https://www.facebook.com/share/19hWobzdw2/" class="btn btn-outline-light btn-sm m-1">
+          <i class="glyphicon glyphicon-thumbs-up"></i> Facebook
+        </a>
+        <a href="https://www.instagram.com/viana_sazta?igsh=Y3Q1OHJxajBwZTB0" class="btn btn-outline-light btn-sm m-1">
+          <i class="glyphicon glyphicon-camera"></i> Instagram
+        </a>
+        <a href=" https://x.com/szt02?t=_pJjrIPrSZgzAYIvevxmSw&s=08" class="btn btn-outline-light btn-sm m-1">
+          <i class="glyphicon glyphicon-retweet"></i> Twitter
+        </a>
+      </div>
+    </div>
+
+    <hr class="bg-light">
+
+    <div class="text-center pb-2">
+      <small>&copy; <?php echo date('Y'); ?> Sistem Informasi Laundry | All Rights Reserved</small>
+    </div>
+  </div>
+</footer>
